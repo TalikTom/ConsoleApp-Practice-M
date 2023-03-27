0182bba6989252b4fd24d676763d44f468e2c8a0
Luka Agic <agic.luke@gmail.com>
2023-03-27 09:06:15 +0200

    Initial commit; Add menu options;

---
+++ ConsoleAppPractice/ConsoleApp/Program.cs
@@ -0,0 +1,61 @@
+﻿MainMenu();
+
+static void MainMenu()
+{
+    Console.Write("\nOptions" +
+        ":\n");
+    Console.Write("1: PARNOST\n2: KVADRATNA\n3: PROSJEK\n4: ZNAMENKE\n5: LOTO\n6: LISTIĆ\n7: OSOBA\n8: PDF\n9: EXIT");
+
+    int input;
+
+    Console.Write("\nInput your choice: ");
+    while (!int.TryParse(Console.ReadLine(), out input))
+    {
+        Console.WriteLine("Invalid input! Please enter a valid number.\n");
+    }
+
+    switch (input)
+    {
+        case 1:
+           
+            break;
+
+        case 2:
+            
+            break;
+
+        case 3:
+           
+            break;
+
+        case 4:
+            
+            break;
+
+        case 6:
+
+            break;
+
+        case 7:
+
+            break;
+
+        case 8:
+
+            break;
+
+        case 9:
+            Console.WriteLine("----------------------------------------------------------- \n");
+            Console.Write("Goodbye\n");
+
+            break;
+
+        default:
+            Console.Clear();
+            Console.Write("Not, fair... Input correct option between 1 and 5\n");
+            MainMenu();
+            break;
+
+
+    }
+}
